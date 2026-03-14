14e4ef51802443516952cb76b9d52c4949abd63f
w1nzeen <w1nzeen.r@gmail.com>
2026-03-14 23:37:48 +0200

    Added task 2

--- lab5/main.py
+++ lab5/main.py
@@ -11,18 +11,20 @@ scl_num = Pin(7)
 WIDTH = 128
 HEIGHT = 128
 
-
 OLED_ADDR = 0x3d  ###Aдреса дисплею на I2C шині
-
 
 i2c = SoftI2C(scl =scl_num, sda = sda_num, freq = 1_000_000)
 sleep(0.1)
 
 OLED = OLED_1inch5(OLED_ADDR, i2c)
 
+''' Кінець налаштувань :) Далі все за вами!'''
+
+
+'''Перша таска "Вивід тексту" '''
 def task1():
     OLED.fill(0)
-    OLED.text("SMARAHDOVE NEBO",1,5,OLED.white)
+    OLED.text("SMARAHDOVE  NEBO",1,5,OLED.white)
     OLED.show()
     sleep(1)
     OLED.text("CHEKAYE NA ZAVTRA",1,20,OLED.white)
@@ -35,17 +37,34 @@ def task1():
     OLED.show()
     sleep(3)
 
+'''Друга таска "Вивід лінії, фігури" '''
+def task2():
+    OLED.fill(0)
+    OLED.hline(19, 11, 93, 15)
+    OLED.show()
+    sleep(1)
+    OLED.vline(111, 12, 51, 15)
+    OLED.show()
+    sleep(1)
+    OLED.line(111, 62, 19, 12, 15) # Формат x, y, x2, y2, colour
+    OLED.show()
+    sleep(1)
+    OLED.rect(21, 45, 44, 15, 15)
+    OLED.show()
+    sleep(1)
+    OLED.rect(21, 65, 44, 13, 15, True)
+    OLED.show()
+    sleep(3)
 
-# OLED.pixel(100,100,15)
 
-# OLED.line(0,0,127,0,1)
-# OLED.hline(0,64,128,1)
-# OLED.vline(64,0,128,1)
 
-# OLED.rect(0, 80,127,90, 8)
-# OLED.rect(0, 90,127,100, 10, True)
+'''Третя таска "Анімація" '''
+def task3():
+    pass
 
-# OLED.show()
 
+'''Основний цикл'''
 while True:
     task1()
+    task2()
+    task3()
